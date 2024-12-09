ff5055d4c888f54008288c34cebaffa8f0f0098c
marcusm0606 <149545275+marcusm0606@users.noreply.github.com>
2024-12-08 20:00:28 -0500

    startup

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+pip install git+https://github.com/CompVis/taming-transformers.git@master
+pip install git+https://github.com/openai/CLIP.git@main
+
+# Install dependencies
+pip install torch torchvision
+pip install transformers==4.19.2 diffusers invisible-watermark
+pip install torch==1.11.0 torchvision==0.12.0
+pip install numpy==1.19.2 albumentations==0.4.3
+pip install opencv-python==4.1.2.30 pudb==2019.2
+pip install invisible-watermark imageio==2.9.0 imageio-ffmpeg==0.4.2
+pip install pytorch-lightning==1.4.2 omegaconf==2.1.1 test-tube>=0.7.5
+pip install streamlit>=0.73.1 einops==0.3.0 torch-fidelity==0.3.0
+pip install torchmetrics==0.6.0 kornia==0.6
+
 
